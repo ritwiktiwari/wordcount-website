06f08b4c1fea5697be524712004c5b7c9c183723
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-18 11:14:39 +0530

    added count route

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def index(request):
     return render(request, 'index.html')
+
+
+def count(request):
+    if request.method == 'GET':
+        return redirect('index')
