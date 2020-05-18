09233ba8890e1be0c25abe71ff03747dffba978e
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-18 12:15:15 +0530

    added word count

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -2,5 +2,4 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('count', views.count, name='count'),
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -2,9 +2,20 @@ from django.shortcuts import render, redirect
 
 
 def index(request):
+    if request.method == 'POST':
+        words = request.POST['fulltext']
+        word_list = words.split()
+        count = len(word_list)
+        lexicon = {}
+        for word in word_list:
+            if word in lexicon:
+                lexicon[word] += 1
+            else:
+                lexicon[word] = 1
+        context = {
+            'words': words,
+            'count': count,
+            'lexicon': lexicon.items(),
+        }
+        return render(request, 'index.html', context)
     return render(request, 'index.html')
-
-
-def count(request):
-    if request.method == 'GET':
-        return redirect('index')
