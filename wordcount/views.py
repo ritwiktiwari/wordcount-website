2ba80a0daf1a95c1d73f41e5366cb0b96f2b8b16
Ritwik Tiwari <ritwiktiwari24@gmail.com>
2020-05-18 09:55:28 +0530

    templates

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse('<h1>Welcome</h1>')
+    return render(request, 'base.html')
